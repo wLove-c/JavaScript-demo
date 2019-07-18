aa8fcf58dfb2c3c4cc313fc8d8cae54ecaef245d
wangbinchang <wangbinchang@do1.com.cn>
2019-07-18 17:22:04 +0800

    更新skudemo

--- sku-demo/src/components/SkuDemo.vue
+++ sku-demo/src/components/SkuDemo.vue
@@ -6,27 +6,71 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'SkuDemo',
   data () {
-    return {}
+    return {
+      arr: [
+        {
+          skuName: '1',
+          skuValuePOS: [
+            { value: '1' },
+            { value: '2' },
+          ]
+        },
+        {
+          skuName: '2',
+          skuValuePOS: [
+            { value: '4' },
+            { value: '5' },
+          ]
+        },
+        {
+          skuName: '3',
+          skuValuePOS: [
+            { value: '8' },
+            { value: '9' },
+          ]
+        }
+      ],
+      listData:[]
+    }
+  },
+  created () {
+    this.createDikaer()
+  },
+  methods: {
+    createDikaer(){
+      let list = this.arr
+      this.listData = this.formatData(list)
+    },
+    formatData (list) {
+      let allArr = []
+      this.doCombination(allArr,list,)
+      return allArr
+    },
+    doCombination ( allArr, list, index=0,arr=[] ) {
+      if (!list[0]) {
+        allArr = []
+        return
+      }
+      list[index].skuValuePOS.map((item) => {
+        let obj = [{
+          skuName:list[index].skuName||'',
+          value:item.value||''
+        }]
+        let newArr =[...arr,...obj]
+        if (list[index + 1]) {
+          this.doCombination(allArr, list, index + 1, newArr)
+        } else {
+          allArr.push(newArr)
+        }
+      })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
